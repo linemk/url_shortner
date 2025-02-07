52d03ac4060dc3e7dcbdeb904f9798ad476c29ed
maks <maks@MacBook-Air.local>
2025-02-08 01:00:38 +0300

    save method

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -15,10 +15,12 @@ type Storage struct {
 
 func New(staragePath string) (*Storage, error) {
 	const op = "storage.sqlite.New"
+
 	db, err := sql.Open("sqlite3", staragePath)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
+
 	stmt, err := db.Prepare(`
 		CREATE TABLE IF NOT EXISTS url(
 		 id INTEGER PRIMARY KEY,
@@ -28,31 +30,38 @@ func New(staragePath string) (*Storage, error) {
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
+
 	_, err = stmt.Exec()
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
+
 	return &Storage{db: db}, nil
 }
+
 func (s *Storage) SaveUrl(urlToSave, alias string) (int64, error) {
 	const op = "storage.sqlite.SaveUrl"
+
 	stmt, err := s.db.Prepare(`INSERT INTO url(alias, url) VALUES(?, ?)`)
 	if err != nil {
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
+
 	res, err := stmt.Exec(urlToSave, alias)
+
 	if err != nil {
 		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
 			return 0, fmt.Errorf("%s: %w", op, storage.ErrUrlExists)
 		}
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
+
 	id, err := res.LastInsertId()
 	if err != nil {
 		return 0, fmt.Errorf("%s:failed to get last insert id: %w", op, err)
 	}
-	return id, nil
 
+	return id, nil
 }
 
 func (s *Storage) GetUrl(alias string) (string, error) {
@@ -64,13 +73,16 @@ func (s *Storage) GetUrl(alias string) (string, error) {
 	}
 
 	var resURL string
+
 	err = stmt.QueryRow(alias).Scan(&resURL)
+
 	if errors.Is(err, sql.ErrNoRows) {
 		return "", storage.ErrUrlNotFound
 	}
+
 	if err != nil {
 		return "", fmt.Errorf("%s: %w", op, err)
 	}
-	return resURL, nil
 
+	return resURL, nil
 }
