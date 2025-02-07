c2dd706f6be55e186236e1e9457ed8317c70b9e7
maks <maks@MacBook-Air.local>
2025-02-08 00:17:04 +0300

    add logger

--- cmd/url-shortner/main.go
+++ cmd/url-shortner/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/linemk/url_shortner/internal/config"
+	mvLogger "github.com/linemk/url_shortner/internal/http-server/middleware/logger"
+	"github.com/linemk/url_shortner/internal/lib/logger/handlers/slogpretty"
 	"github.com/linemk/url_shortner/internal/lib/logger/sl"
 	"github.com/linemk/url_shortner/internal/storage/sqlite"
 	"log/slog"
 	"os"
-
-	"github.com/go-chi/chi/v5"
 )
 
 const (
@@ -32,8 +34,15 @@ func main() {
 		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
+
 	router := chi.NewRouter()
 
+	router.Use(middleware.RequestID)
+	router.Use(mvLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	_ = storage
 }
 
 func setUpLogger(env string) *slog.Logger {
@@ -42,7 +51,7 @@ func setUpLogger(env string) *slog.Logger {
 	switch env {
 	case envLocal:
 
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envProd:
@@ -50,4 +59,16 @@ func setUpLogger(env string) *slog.Logger {
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
